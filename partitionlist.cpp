8e35043d99fc414f35a3bfc1ebd47d16fb69c501
Purnima Katiyar <75112217+purnimakatiyar@users.noreply.github.com>
2022-07-22 19:47:11 +0530

    Add files via upload

---
+++ partitionlist.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+       if(head == NULL || head->next == NULL) return head;
+        ListNode* temp = head, *cur = head;
+        while(temp){
+            if(temp->val < x){
+                int value = temp->val;
+                ListNode* dup = cur;
+                while(dup){
+                    int y = dup->val;
+                    dup->val = value;
+                    value = y;
+                    if(dup == temp) break;
+                    dup=dup->next;
+                }
+                cur = cur->next;
+            }
+            temp = temp->next;
+        }
+        return head; 
+    }
+};
